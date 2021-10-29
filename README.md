a53091e87aef4e5159aa40ffb614eb9c7797b50d
Patrique Legault <patrique.legault@gmail.com>
2021-10-29 10:47:39 -0400

    Adding discover-ip commands

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# network-commands
-A set of bash files to help identify information in a network
+# Network Commands
+
+A set of bash files to help identify information in a network.
+
+## Purpose
+
+I have a local Raspberry Pi at home that I use for local development and sometimes the IP address of it changes within my local network, when this happens I don't have a way to locate it without logging in to my router. 
+
+In order to circumvent this long process I decided to log some of commands that could help determine and locate my Raspberry Pi on my network if this ever happened.
+
---
+++ discover-ip.sh
@@ -0,0 +1,19 @@
+ # This command will help discover IP addresses on a local network
+
+ # Prior to using this script ake sure to have 
+ # arp-scan installed on your laptop
+ #
+ # Installation:
+ # Mac OSX: brew install arp-scan
+ # Linux (via apt-get): sudo apt install arp-scan
+ #
+ # How to use:
+ # ./discover-ip.sh <interface>
+ # In order to discover the necessary interface to query
+ # execute the ifconfig command from your cli in order
+ # to list all of the available interfaces on your machine
+ # copy the correct interface you wish to query and then 
+ # provide that as a parameter to this script
+ #
+ # **Note:** You will need to chmod the file in order to execute it on your laptop
+ sudo arp-scan --interface=$1 --localnet
